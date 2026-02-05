80949a96dbd69376e1b278c84138635b8dea1bea
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2026-02-04 22:31:57 -0600

    initial commit with project setup

---
+++ src/main/java/com/example/vinyl_record_collection_tracker/VinylRecordCollectionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinyl_record_collection_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylRecordCollectionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylRecordCollectionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vinyl_record_collection_tracker/VinylRecordCollectionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinyl_record_collection_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylRecordCollectionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
